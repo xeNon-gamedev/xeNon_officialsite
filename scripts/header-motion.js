d00ca4bc2920727a3f7fe5aebecbc24271c5d63e
ぐらむ/gramme <yugo.08172008@gmail.com>
2025-04-19 19:28:31 +0900

    I have overhauled our entire website to make it lighter.

--- scripts/header-motion.js
+++ scripts/header-motion.js
@@ -1,8 +1,1 @@
-const ham = document.querySelector('#js-hamburger'); //js-hamburgerの要素を取得し、変数hamに格納
-const nav = document.querySelector('#header-menu'); //js-navの要素を取得し、変数navに格納
-
-ham.addEventListener('click', function () { //ハンバーガーメニューをクリックしたら
-    ham.classList.toggle('active'); // ハンバーガーメニューにactiveクラスを付け外し
-    nav.classList.toggle('active'); // ナビゲーションメニューにactiveクラスを付け外し
-
-});
+const ham=document.querySelector('#js-hamburger');const nav=document.querySelector('#header-menu');ham.addEventListener('click',function(){ham.classList.toggle('active');nav.classList.toggle('active')})
